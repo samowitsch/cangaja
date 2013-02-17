8e4d0b1ab76cef6db3da867ea67750082a706d19
Christian Sonntag <info@motions-media.de>
2013-02-17 17:52:41 +0100

    prepare more ;o)

--- media/engine/b2d-rope.js
+++ media/engine/b2d-rope.js
@@ -45,6 +45,67 @@ CG.B2DEntity.extend('B2DRope', {
 //        return this
 
     },
+
+/*
+
+ Method CreateRope:Void(world:b2World, img:Image, x:Float, y:Float, length:Float, segments:Int, segHeight:float, physScale:Float)
+     Self.physScale = physScale
+     Self.entityType="rope"
+     Self.img = img
+     If img <> Null
+     Self.img.SetHandle(Self.img.Width() / 2, Self.img.Height() / 2)
+     Endif
+
+     Local anchor:b2Vec2	= New b2Vec2()
+     Local prevBody:b2Body
+     Local segWidth:Float = ((length-x)/segments)/2
+
+     '// RopeStart
+     Self.fixtureDef		= New b2FixtureDef()
+     Self.bodyShapeCircle= New b2CircleShape()
+     Self.bodyDef		= New b2BodyDef()
+     Self.bodyShapeCircle.m_radius= segHeight / physScale
+     Self.fixtureDef.density 	= 1.0
+     Self.fixtureDef.restitution = 0.2
+     Self.fixtureDef.friction 	= 0.2
+     Self.fixtureDef.shape=Self.bodyShapeCircle
+     Self.bodyDef.position.Set(x / physScale, y / physScale)
+     Self.bodyGroup[0] = world.CreateBody(bodyDef)
+     Self.bodyGroup[0].CreateFixture(fixtureDef)
+     prevBody = Self.bodyGroup[0]
+
+     '// RopeSegments
+     Self.fixtureDef		= New b2FixtureDef()
+     Self.bodyShapePoly	= New b2PolygonShape()
+     Self.bodyDef		= New b2BodyDef()
+     Self.bodyShapePoly.SetAsBox(segWidth / physScale, segHeight / physScale)
+     Self.bodyDef.type 	= b2Body.b2_Body
+     Self.fixtureDef.shape 	= bodyShapePoly
+     Self.fixtureDef.density = 1.0
+     Self.fixtureDef.restitution = 0.2
+     Self.fixtureDef.friction= 0.2
+     Self.jointDef 		= New b2RevoluteJointDef()
+     Self.jointDef.lowerAngle = -25 / (180/Constants.PI)
+     Self.jointDef.upperAngle = 25 / (180/Constants.PI)
+     Self.jointDef.enableLimit = True
+
+     For Local i:Int = 0 To segments-1
+     bodyDef.position.Set(((x+segWidth) + (segWidth*2) * i) / physScale, y / physScale)
+     Self.bodyGroup[i+1] = world.CreateBody(bodyDef)
+     Self.bodyGroup[i+1].CreateFixture(fixtureDef)
+     anchor.Set((x + (segWidth*2) * i)/ physScale, y / physScale)
+     jointDef.Initialize(prevBody, Self.bodyGroup[i+1], anchor)
+     world.CreateJoint(jointDef)
+     prevBody = Self.bodyGroup[i+1]
+     Self.bodyCount = i+1
+     Next
+ End
+
+
+*/
+
+
+
     draw:function () {
         //TODO rewrite for rope
 
@@ -60,6 +121,64 @@ CG.B2DEntity.extend('B2DRope', {
 //        }
 //        Game.b_ctx.restore()
     }
+
+
+/*
+
+ Method Draw:Void(ratio:Float = 1.0)
+     Local f :b2Fixture
+     Local s :b2Shape
+     Local xf :b2Transform
+
+     If Self.debugDraw = True
+     If Self.bodyCount > 0 'Draw MultiBody
+     For Local i:Int=0 to Self.bodyCount
+     xf = Self.bodyGroup[i].m_xf
+     f = Self.bodyGroup[i].GetFixtureList()
+     While ( f <> Null )
+     s = f.GetShape()
+     DrawShape(s, xf, Self.debugColor)
+     f = f.m_next
+     End
+     Next
+     Else
+     xf = Self.body.m_xf
+     f = Self.body.GetFixtureList()
+     While ( f <> Null )
+     s = f.GetShape()
+     DrawShape(s, xf, Self.debugColor)
+     f = f.m_next
+     End
+     Endif
+     SetColor(255,255,255)
+     ElseIf Self.img <> Null
+     If Self.bodyCount > 0
+     If Self.entityType="bridge" 'Draw Bridge
+     For Local i:Int=2 to Self.bodyCount
+     Local x:Float = Self.bodyGroup[i].GetPosition().x
+     Local y:Float = Self.bodyGroup[i].GetPosition().y
+     Local r:Float	= RadToDeg(Self.bodyGroup[i].GetAngle()) * -1
+     DrawImage(Self.img, x * Self.physScale, y * Self.physScale, r, 1.0, 1.0, 0)
+     Next
+     EndIf
+     If Self.entityType="rope" 'Draw Rope
+     For Local i:Int=1 to Self.bodyCount
+     Local x:Float = Self.bodyGroup[i].GetPosition().x
+     Local y:Float = Self.bodyGroup[i].GetPosition().y
+     Local r:Float	= RadToDeg(Self.bodyGroup[i].GetAngle()) * -1
+     DrawImage(Self.img, x * Self.physScale, y * Self.physScale, r, 1.0, 1.0, 0)
+     Next
+     EndIf
+     Else
+     Local x:Float	= Self.body.GetPosition().x
+     Local y:Float	= Self.body.GetPosition().y
+     Local r:Float	= RadToDeg(Self.body.GetAngle()) * -1
+     DrawImage(Self.img, x * Self.physScale, y * Self.physScale, r, 1.0, 1.0, 0)
+     EndIf
+     Endif
+ End
+
+*/
 
 })
 
