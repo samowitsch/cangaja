1ebcefc721a3ebf23c83ca43c0dde86e41c47609
Christian Sonntag <info@motions-media.de>
2013-11-21 20:58:48 +0100

    added some tests for the webgl-2d.js lib

---
+++ demo/demo-particle-emitter-webgl-2d.js
@@ -0,0 +1,234 @@
+var renderStats
+
+var mainscreen, mainlayer, smokey
+
+var mousex = 0
+var mousey = 0
+var mousedown = false
+var mouseup
+var tp = new CG.AtlasTexturePacker()
+
+
+//waiting to get started ;o)
+window.onload = function () {
+
+    //create canvas element programaticaly
+    can = document.createElement('canvas')
+    can.width = 640
+    can.height = 480
+    can.id = 'canvas'
+    document.body.appendChild(can)
+
+    can.addEventListener("mousedown", function (e) {
+        mousedown = true;
+    }, true);
+
+    can.addEventListener("mouseup", function () {
+        mousedown = false;
+    }, true);
+
+    can.addEventListener('mousemove', function (evt) {
+        var rect = can.getBoundingClientRect(), root = document.documentElement;
+        mousex = evt.clientX - canvas.offsetLeft;
+        mousey = evt.clientY - canvas.offsetTop;
+    }, false);
+
+    Game.preload()
+};
+
+// the Game object
+Game = (function () {
+    var Game = {
+        path: '',
+        fps:60,
+        width:640,
+        height:480,
+        width2:640 / 2,
+        height2:480 / 2,
+        bound:new CG.Bound(0, 0, 640, 480).setName('game'),
+        canvas:{},
+        ctx:{},
+        b_canvas:{},
+        b_ctx:{},
+        asset:new CG.MediaAsset(),
+        director:new CG.Director(),
+        renderer: new CG.CanvasRenderer(),
+        delta:new CG.Delta(60),
+        preload:function () {
+            //canvas for ouput
+            Game.canvas = Game.b_canvas = document.getElementById("canvas")
+            WebGL2D.enable(Game.b_canvas)
+            Game.ctx = Game.b_ctx = Game.canvas.getContext("2d")
+
+            //Asset preloading font files
+            Game.asset.addFont('media/font/small.txt', 'small', 'small')
+                .addFont('media/font/abadi_ez.txt', 'abadi')
+                .addImage('media/img/glowball-50.png', 'glowball')
+                //texturepacker
+                .addImage('media/img/texturepacker.png', 'texturepacker')
+                .addJson('media/img/texturepacker.json', 'texturepacker-json')
+
+                .startPreLoad()
+        },
+        create:function () {
+
+            //create texturepacker image in asset
+            tp.loadJson(Game.asset.getJsonByName('texturepacker-json'))
+
+            //put the texturepacker TPImages to the asset
+            Game.asset.images.push.apply(Game.asset.images, tp.getAtlasImages())
+
+            //            font = new CG.Font().loadFont(Game.asset.getFontByName('small'))
+            abadi = new CG.Font().loadFont(Game.asset.getFontByName('abadi'))
+            small = new CG.Font().loadFont(Game.asset.getFontByName('small'))
+
+            //screen and layer
+            mainscreen = new CG.Screen('mainscreen')
+            mainlayer = new CG.Layer('mainlayer')
+
+            //add screen to Director
+            Game.director.addScreen(mainscreen.addLayer(mainlayer))
+
+
+            mainlayer.addElement(new CG.Emitter(new CG.Point(Game.width2, Game.height2))
+                .setName('splashi')
+                .setParticleSpeed(0)
+                .setPLifetime(40)
+                .setMaxParticles(100)
+                .setGravity(0)
+                .setCreationTime(1)
+                .initAsRectangle(Game.asset.getImageByName('splash'), Game.width, Game.height)
+                .setProtation(0)
+                //        .setEmitterPosition(new CG.Point(320,240))
+                .activateFadeout()
+            )
+
+            mainlayer.addElement(new CG.Emitter(new CG.Point(Game.width2, Game.height + 20))
+                .setName('sunny')
+                .setParticleSpeed(2)
+                .setMaxParticles(100)
+                .setGravity(0.02)
+                .initAsLine(Game.asset.getImageByName('sonne-50'), Game.height, CG.UP)
+                .setProtation(2)
+                //        .setEmitterPosition(new CG.Point(320,500))
+                .activateFadeout()
+            )
+
+
+            mainlayer.addElement(new CG.Emitter(new CG.Point(Game.width2, -20))
+                .setName('rainy')
+                .setParticleSpeed(2)
+                .setMaxParticles(100)
+                .setGravity(0.03)
+                .setCreationTime(1)
+                .initAsLine(Game.asset.getImageByName('raindrop'), Game.height, CG.DOWN)
+                //        .setEmitterPosition(new CG.Point(320,-20))
+                .activateFadeout()
+            )
+
+
+            mainlayer.addElement(new CG.Emitter(new CG.Point(-25, Game.height2))
+                .setName('glowy')
+                .setGravity(0)
+                .setParticleSpeed(2)
+                .setMaxParticles(100)
+                .setCreationTime(1)
+                .setProtation(2)
+                .initAsLine(Game.asset.getImageByName('glowball-50'), 40, CG.RIGHT)
+                //        .setEmitterPosition(new CG.Point(-25,240))
+                .activateFadeout()
+            )
+
+            mainlayer.addElement(new CG.Emitter(new CG.Point(Game.width + 15, Game.height2))
+                .setName('bally')
+                .setParticleSpeed(2)
+                .setMaxParticles(100)
+                .setProtation(-1)
+                .setCreationTime(1)
+                .setGravity(0)
+                .initAsLine(Game.asset.getImageByName('basketball-25'), 40, CG.LEFT)
+                //        .setEmitterPosition(new CG.Point(655,240))
+                .activateFadeout()
+            )
+
+            mainlayer.addElement(new CG.Emitter(new CG.Point(Game.width2, Game.height2))
+                .setName('explodi-stars')
+                .activateFadeout()
+                .setMaxParticles(100)
+                .setProtation(2)
+                .setGravity(0)
+                .initAsExplosion(Game.asset.getImageByName('powerstar75'), -2, 2)
+                //        .setEmitterPosition(new CG.Point(320, 240))
+            )
+
+            smokey = new CG.Emitter(new CG.Point(Game.width2, Game.height2))
+                .setName('smokey')
+                .activateFadeout()
+                .setMaxParticles(100)
+                .setCreationTime(1)
+                .setGravity(-0.05)
+                .initAsPoint(Game.asset.getImageByName('smoke50'))
+
+            mainlayer.addElement(smokey)
+
+
+            mainlayer.addElement(new CG.Emitter(new CG.Point(160, 120))
+                .setName('smokey')
+                .setMaxParticles(100)
+                .activateFadeout()
+                .setGravity(0.25)
+                .setProtation(5)
+                .setParticleSpeed(0.5)
+                .setPLifetime(50)
+                .setCreationTime(1)
+                .initAsCorona(Game.asset.getImageByName('littlestar'), 100)
+                //        .setEmitterPosition(new CG.Point(160, 120))
+            )
+
+
+            renderStats = new Stats()
+            document.body.appendChild(renderStats.domElement)
+
+            Game.loop()
+        },
+        loop:function () {
+            requestAnimationFrame(Game.loop);
+            if (Game.asset.ready == true) {
+                Game.run();
+            }
+        },
+        run:function () {
+            Game.update()
+            Game.draw()
+        },
+        update:function () {
+            //update here what ever you want
+
+            smokey.position._x = mousex
+            smokey.position._y = mousey
+            Game.director.update()
+        },
+        draw:function () {
+            Game.b_ctx.clearRect(0, 0, Game.bound.width, Game.bound.height)
+            var xpos = 10
+            var ypos = 10
+
+            Game.director.draw()
+
+            abadi.drawText('cangaja - Canvas Game JavaScript FW', xpos, ypos)
+
+            small.drawText('Particle/Emitter class example.', xpos, ypos + 50)
+
+//            Game.ctx.drawImage(Game.b_canvas, 0, 0)
+//            Game.b_ctx.clearRect(0, 0, Game.bound.width, Game.bound.height)
+
+            renderStats.update();
+        },
+        touchinit:function () {
+        },
+        touchhandler:function () {
+        }
+    }
+
+    return Game
+}())
---
+++ demo/demo-sprite-webgl-2d.js
@@ -0,0 +1,168 @@
+var renderStats
+
+var mainscreen, mainlayer
+
+var mousex = 0
+var mousey = 0
+var mousedown = false
+var mouseup
+
+
+//waiting to get started ;o)
+window.onload = function () {
+
+    //create canvas element programaticaly
+    can = document.createElement('canvas')
+    can.width = 640
+    can.height = 480
+    can.id = 'canvas'
+    document.body.appendChild(can)
+
+    Game.preload()
+};
+
+// the Game object
+Game = (function () {
+    var Game = {
+        path: '',
+        fps: 60,
+        width: 640,
+        height: 480,
+        width2: 640 / 2,
+        height2: 480 / 2,
+        bound: new CG.Bound(0, 0, 640, 480).setName('game'),
+        canvas: {},
+        ctx: {},
+        b_canvas: {},
+        b_ctx: {},
+        asset: new CG.MediaAsset(),
+        director: new CG.Director(),
+        renderer: new CG.CanvasRenderer(),
+        delta: new CG.Delta(60),
+        preload: function () {
+            //canvas for ouput
+            Game.canvas = Game.b_canvas = document.getElementById("canvas")
+            WebGL2D.enable(Game.b_canvas)
+            Game.ctx = Game.b_ctx = Game.b_canvas.getContext("webgl-2d")
+
+            //Asset preloading font files
+            Game.asset
+
+                .addImage('media/font/small.png', 'small')
+                .addFont('media/font/small.txt', 'small')
+
+                .addImage('media/font/abadi_ez.png', 'abadi')
+                .addFont('media/font/abadi_ez.txt', 'abadi')
+
+                .addImage('media/img/glowball-50.png', 'glowball')
+
+                .startPreLoad()
+        },
+        create: function () {
+
+            //            font = new CG.Font().loadFont(Game.asset.getFontByName('small'))
+            abadi = new CG.Font().loadFont(Game.asset.getFontByName('abadi'))
+            small = new CG.Font().loadFont(Game.asset.getFontByName('small'))
+
+            //screen and layer
+            mainscreen = new CG.Screen('mainscreen')
+            mainlayer = new CG.Layer('mainlayer')
+
+            //add screen to Director
+            Game.director.addScreen(mainscreen.addLayer(mainlayer))
+
+            //sprite 1
+            spr1 = new CG.Sprite(Game.asset.getImageByName('glowball'), new CG.Point(50, 100))
+            spr1.name = 'spr1'
+            mainlayer.addElement(spr1)
+
+            //sprite 2
+            spr2 = new CG.Sprite(Game.asset.getImageByName('glowball'), new CG.Point(200, 100))
+            spr2.rotationspeed = 1
+            spr2.name = 'spr2'
+            mainlayer.addElement(spr2)
+
+
+            //sprite 3
+            spr3 = new CG.Sprite(Game.asset.getImageByName('glowball'), new CG.Point(400, 100))
+            spr3.alpha = 0.5
+            spr3.xscale = 0.5
+            spr3.yscale = 0.5
+            spr3.name = 'spr3'
+            mainlayer.addElement(spr3)
+
+            //sprite 4
+            spr4 = new CG.Sprite(Game.asset.getImageByName('glowball'), new CG.Point(50, 200))
+            spr4.xspeed = 2
+            spr4.boundsMode = 'bounce'
+            spr4.name = 'spr4'
+            mainlayer.addElement(spr4)
+
+            //sprite 5
+            spr5 = new CG.Sprite(Game.asset.getImageByName('glowball'), new CG.Point(50, 300))
+            spr5.xspeed = 2
+            spr5.boundsMode = 'slide'
+            spr5.name = 'spr5'
+            mainlayer.addElement(spr5)
+
+            //sprite 6
+            spr6 = new CG.Sprite(Game.asset.getImageByName('glowball'), new CG.Point(50, 300))
+            spr6.xspeed = 1
+            spr6.yspeed = 1
+            spr6.bound = new CG.Bound(0, 400, 640, 80)
+            spr6.boundsMode = 'bounce'
+            spr6.name = 'spr6'
+            mainlayer.addElement(spr6)
+
+
+            renderStats = new Stats()
+            document.body.appendChild(renderStats.domElement)
+
+            Game.loop()
+        },
+        loop: function () {
+            requestAnimationFrame(Game.loop);
+            if (Game.asset.ready == true) {
+                Game.run();
+            }
+        },
+        run: function () {
+            Game.update()
+            Game.draw()
+        },
+        update: function () {
+            //update here what ever you want
+            Game.director.update()
+        },
+        draw: function () {
+            Game.b_ctx.clearRect(0, 0, Game.bound.width, Game.bound.height)
+            var xpos = 10
+            var ypos = 10
+
+            Game.director.draw()
+
+            abadi.drawText('cangaja - Canvas Game JavaScript FW', xpos, ypos)
+
+            small.drawText('Sprite class example.', xpos, ypos + 50)
+
+
+            small.drawText('SIMPLE SPRITE', spr1.position.x + 20, spr1.position.y + 20)
+            small.drawText('SPRITE WITH ROTATION', spr2.position.x + 20, spr2.position.y + 20)
+            small.drawText('ALPHA 0.5 / SCALE 0.5', spr3.position.x + 20, spr3.position.y + 20)
+            small.drawText('MODE BOUNCE', spr4.position.x + 20, spr4.position.y + 20)
+            small.drawText('MODE SLIDE', spr5.position.x + 20, spr5.position.y + 20)
+            small.drawText('CUSTOM BOUND', spr6.position.x + 20, spr6.position.y + 20)
+
+//            Game.ctx.drawImage(Game.b_canvas, 0, 0)
+//            Game.b_ctx.clearRect(0, 0, Game.bound.width, Game.bound.height)
+
+            renderStats.update();
+        },
+        touchinit: function () {
+        },
+        touchhandler: function () {
+        }
+    }
+
+    return Game
+}())
