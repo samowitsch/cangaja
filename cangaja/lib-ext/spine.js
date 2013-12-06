ae90c970240ce631665ad99806adb7cbdc774c5b
Christian Sonntag <info@motions-media.de>
2013-12-06 22:25:56 +0100

    wtf? is this a feature in the spine-runtimes or is it a bug in cangajas canvas renderer? why change worldX/worldY to negative values?

--- cangaja/lib-ext/spine.js
+++ cangaja/lib-ext/spine.js
@@ -84,8 +84,8 @@ spine.Bone.prototype = {
 			}
 			this.worldRotation = this.data.inheritRotation ? parent.worldRotation + this.rotation : this.rotation;
 		} else {
-			this.worldX = flipX ? -this.x : this.x;
-			this.worldY = (flipY != spine.Bone.yDown) ? -this.y : this.y;
+			this.worldX = this.x; // flipX ? -this.x : this.x;
+			this.worldY = this.y; //(flipY != spine.Bone.yDown) ? -this.y : this.y;
 			this.worldScaleX = this.scaleX;
 			this.worldScaleY = this.scaleY;
 			this.worldRotation = this.rotation;
