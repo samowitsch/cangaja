5a9f837d40a9c49a6a0d5f893e4d0eef50225fca
Christian Sonntag <info@motions-media.de>
2014-12-25 00:08:41 +0100

    - removed property animated - bugfix and improvement in method updateAnimtion, removed try/catch block

--- cangaja/tilemap/map.js
+++ cangaja/tilemap/map.js
@@ -59,16 +59,6 @@ CG.Entity.extend('Map', {
          */
         this.mapOffset = new CG.Point(0, 0)
         /**
-         * @description
-         *
-         * If set to true the map is being updated with method updateAnimation.
-         * See also method description of updateAnimation!
-         *
-         * @property animated
-         * @type {Boolean}
-         */
-        this.animated = false //performance eater if true ;o(
-        /**
          * @property animDelayFactor
          * @type {Number}
          */
@@ -246,7 +236,6 @@ CG.Entity.extend('Map', {
      * @param xmlfile {string/object} xmlfile path or mediaasset object with data of tiled map xml
      */
     loadMapXml: function (xmlfile) {
-        this.animated = false
         this.layers = []
 
         //from asset
@@ -416,12 +405,10 @@ CG.Entity.extend('Map', {
                         } else if (elem == 'anim_delay') {
                             tprop.animDelay = parseInt(value)
                             tprop.delayTimer = time
-                            this.animated = true
                         } else if (elem == 'anim_direction') {
                             tprop.animDirection = parseInt(value)
                         } else if (elem == 'anim_next') {
                             tprop.animNext = parseInt(value)
-                            tprop.animated = true
                         }
                     }
                 }
@@ -441,7 +428,6 @@ CG.Entity.extend('Map', {
      * @param jsonfile {string/object} jsonfile path or mediaasset object with data of tiled map xml
      */
     loadMapJson: function (jsonfile) {
-        this.animated = false
         this.layers = []
 
         //from asset
@@ -547,11 +533,7 @@ CG.Entity.extend('Map', {
             tprop.name = tile.name
             tprop.animDelay = parseInt(tile.anim_delay)
             tprop.delayTimer = (tprop.animDelay > 0) ? time : 0
-            tprop.animated = (tprop.animDelay > 0) ? true : false
             tprop.animNext = parseInt(tile.anim_next)
-            if (tprop.animDelay > 0) {
-                this.animated = true
-            }
             tprop.animDirection = parseInt(tile.anim_direction)
             this.tileproperties[id] = tprop
 
@@ -855,34 +837,27 @@ CG.Entity.extend('Map', {
      * @method updateAnimation
      */
     updateAnimation: function () {
-        // update if map is visible
-        if (this.visible && this.animated) {
-            if (this.layers.length > 0) {
-                for (var layer = 0, l = this.layers.length; layer < l; layer++) {
-                    var newtime = new Date().getTime()
-                    for (t = 0; t < this.layers[layer].tiles.length; t++) {
-                        var tile = this.layers[layer].tiles[t]
-                        if (tile > 0) {
-                            try {
-                                var tprop = this.tileproperties[tile - 1]
-                                if (tprop.animated && tprop.animDirection != 0) {
-                                    if (newtime > (tprop.delayTimer + (tprop.animDelay / this.animDelayFactor))) {
-                                        switch (tprop.animDirection) {
-                                            case 1:
-                                                this.layers[layer].tiles[t] += tprop.animNext
-                                                this.tileproperties[tile - 1 + tprop.animNext].delayTimer = newtime
-                                                break
-                                            case -1:
-                                                this.layers[layer].tiles[t] -= tprop.animNext
-                                                this.tileproperties[tile - 1 - tprop.animNext].delayTimer = newtime
-                                                break
-                                            default:
-                                                break
-                                        }
-                                    }
+        if (this.layers.length > 0) {
+            for (var layer = 0, l = this.layers.length; layer < l; layer++) {
+                var newtime = new Date().getTime()
+                for (var t = 0, tl = this.layers[layer].tiles.length; t < tl; t++) {
+                    var tile = this.layers[layer].tiles[t]
+                    if (tile > 0) {
+                        var tprop = this.tileproperties[tile - 1]
+                        if (tprop) {
+                            if (newtime > (tprop.delayTimer + (tprop.animDelay / this.animDelayFactor))) {
+                                switch (tprop.animDirection) {
+                                    case 1:
+                                        this.layers[layer].tiles[t] += tprop.animNext
+                                        this.tileproperties[tile - 1 + tprop.animNext].delayTimer = newtime
+                                        break
+                                    case -1:
+                                        this.layers[layer].tiles[t] -= tprop.animNext
+                                        this.tileproperties[tile - 1 - tprop.animNext].delayTimer = newtime
+                                        break
+                                    default:
+                                        break
                                 }
-                            } catch (e) {
-
                             }
                         }
                     }
